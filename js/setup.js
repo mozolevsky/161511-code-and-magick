8800efb3896966778ff3a13df372e81a2b484e4c
mozolevsky <alf.rulit@gmail.com>
2017-01-29 21:28:57 +0300

    Made esLint tests, made vailidation with html5 attrs, code refactoring

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,4 @@
-'use strict'
-
-//добавить стили для валидации, анимации на плавную смену цветов и скрытие/появление форм
+'use strict';
 
 function getElement(selector) {
   return document.querySelector(selector);
@@ -10,85 +8,41 @@ var userIcon = getElement('.setup-open');
 var wizardForm = getElement('.setup');
 var wizardFormClose = getElement('.setup-close');
 
-var nameInput = getElement('.setup-user-name');
-
 var wizardCoat = getElement('#wizard-coat');
-var colorsOfTheCoat = ["rgb(101, 137, 164)",
-                        "rgb(241, 43, 107)",
-                        "rgb(146, 100, 161)",
-                        "rgb(56, 159, 117)",
-                        "rgb(215, 210, 55)",
-                        "rgb(0, 0, 0)"];
+var colorsOfTheCoat = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 
 var wizardEyes = getElement('#wizard-eyes');
-var colorsOfTheEyes = ["black", "red", "blue", "yellow", "green"];
+var colorsOfTheEyes = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var fireBall = getElement('.setup-fireball-wrap');
-var fireBallColors = ["#ee4830", "#30a8ee", "#5ce6c0", "#e848d5", "#e6e848"];
+var fireBallColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
-function showElement() {
-  this.classList.remove('invisible');
+function showElement(element) {
+  element.classList.remove('invisible');
 }
 
-function hideElement() {
-  this.classList.add('invisible');
+function hideElement(element) {
+  element.classList.add('invisible');
 }
 
-userIcon.addEventListener("click", function() {
-  showElement.call(wizardForm);
+userIcon.addEventListener('click', function () {
+  showElement(wizardForm);
 });
 
-wizardForm.addEventListener("click", function(event) {
-  var target = event.target;
-
-  if (target == wizardFormClose) hideElement.call(wizardForm);
-
-  if (target.parentNode == wizardCoat) {
-    wizardCoat.style.fill = getColor(colorsOfTheCoat);
-  }
-
-  if (target.parentNode == wizardEyes) {
-    wizardEyes.style.fill = getColor(colorsOfTheEyes);
-  }
-
-  if (target.parentNode == fireBall) {
-    fireBall.style.background = getColor(fireBallColors);
-  }
+wizardFormClose.addEventListener('click', function () {
+  hideElement(wizardForm);
 });
 
-function createAlertBlock() {
-  var messageBlock = document.createElement("div");
-  messageBlock.className = "alert-error";
-  return messageBlock;
-}
+wizardCoat.addEventListener('click', function () {
+  wizardCoat.style.fill = getColor(colorsOfTheCoat);
+});
 
-function checkInput() {
-    var alertBlock = document.querySelectorAll('.alert-error') || '';
+wizardEyes.addEventListener('click', function () {
+  wizardEyes.style.fill = getColor(colorsOfTheEyes);
+});
 
-  if (!this.value || this.value.length > 50) {
-      this.style.border = "1px solid red";
-      var alertBlock = createAlertBlock(this);
-      var messageText = "";
-
-      if (!this.value) messageText = "Вы не ввели имя персонажа";
-      if (this.value.length > 50) messageText = "Имя должно содержать до 50 символов";
-
-      alertBlock.innerHTML = messageText;
-      return this.parentElement.appendChild(alertBlock);
-  }
-
-  if (alertBlock) {
-    this.style.border = "";
-
-    for (var i = 0; i < alertBlock.length; i++) {
-      this.parentNode.removeChild(alertBlock[i]);
-    }
-
-  }
-}
-
-nameInput.addEventListener("blur", function () {
-  checkInput.call(nameInput);
+fireBall.addEventListener('click', function () {
+  fireBall.style.background = getColor(fireBallColors);
 });
 
 function getColor(colorsArray) {
