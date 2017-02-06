c81bfdae9ae08e9db153a7bf2a451ae747fe29d9
mozolevsky <alf.rulit@gmail.com>
2017-02-06 17:27:09 +0300

    Made additional abilities for disabled people

--- js/setup.js
+++ js/setup.js
@@ -7,6 +7,10 @@ function getElement(selector) {
 var userIcon = getElement('.setup-open');
 var wizardForm = getElement('.setup');
 var wizardFormClose = getElement('.setup-close');
+var wizardFormSaveBtn = getElement('.setup-submit');
+
+var ENTER_KEY_CODE = 13;
+var ESC_KEY_CODE = 27;
 
 var wizardCoat = getElement('#wizard-coat');
 var colorsOfTheCoat = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
@@ -19,18 +23,50 @@ var fireBallColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
 function showElement(element) {
   element.classList.remove('invisible');
+  element.setAttribute('aria-hidden', false);
 }
 
 function hideElement(element) {
   element.classList.add('invisible');
+  element.setAttribute('aria-hidden', true);
 }
+
+var checkKeyCode = function (evt, key) {
+  return (evt.keyCode && evt.keyCode === key);
+};
+
+var listenEscOnWizardForm = function () {
+  document.addEventListener('keydown', function (event) {
+    if (checkKeyCode(event, ESC_KEY_CODE)) {
+      hideElement(wizardForm);
+      userIcon.setAttribute('aria-pressed', false);
+    }
+  });
+};
 
 userIcon.addEventListener('click', function () {
   showElement(wizardForm);
+  listenEscOnWizardForm();
+});
+
+userIcon.addEventListener('keydown', function (event) {
+  if (checkKeyCode(event, ENTER_KEY_CODE)) {
+    showElement(wizardForm);
+    listenEscOnWizardForm();
+    userIcon.setAttribute('aria-pressed', true);
+  }
+});
+
+wizardFormClose.addEventListener('keydown', function (event) {
+  if (checkKeyCode(event, ENTER_KEY_CODE)) {
+    hideElement(wizardForm);
+    userIcon.setAttribute('aria-pressed', false);
+  }
 });
 
 wizardFormClose.addEventListener('click', function () {
   hideElement(wizardForm);
+  userIcon.setAttribute('aria-pressed', false);
 });
 
 wizardCoat.addEventListener('click', function () {
@@ -43,6 +79,18 @@ wizardEyes.addEventListener('click', function () {
 
 fireBall.addEventListener('click', function () {
   fireBall.style.background = getColor(fireBallColors);
+});
+
+wizardFormSaveBtn.addEventListener('click', function () {
+  hideElement(wizardForm);
+  userIcon.setAttribute('aria-pressed', false);
+});
+
+wizardFormSaveBtn.addEventListener('keydown', function () {
+  if (checkKeyCode(event, ENTER_KEY_CODE)) {
+    hideElement(wizardForm);
+    userIcon.setAttribute('aria-pressed', false);
+  }
 });
 
 function getColor(colorsArray) {
